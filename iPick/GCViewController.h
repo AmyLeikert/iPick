5fe875760314e40b568304bb198a134de5e02995
DetroitLabsUser <detroitlabsuser@DetroitLabsUsers-MacBook-Pro.local>
2014-03-06 09:47:52 -0500

    Initial Commit

---
+++ iPick/GCViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCViewController.h
+//  iPick
+//
+//  Created by DetroitLabsUser on 3/6/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCViewController : UIViewController
+
+@end
---
+++ iPick/GCViewController.m
@@ -0,0 +1,29 @@
+//
+//  GCViewController.m
+//  iPick
+//
+//  Created by DetroitLabsUser on 3/6/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import "GCViewController.h"
+
+@interface GCViewController ()
+
+@end
+
+@implementation GCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
